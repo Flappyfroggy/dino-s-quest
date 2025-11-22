a586ec8487d64ed2854266f19c7fc44b00491d8a
Flap <shuruiguo1@gmail.com>
2025-11-22 21:07:06 +1100

    ui for the first quest - making it appear does not work

--- billboard.gd
+++ billboard.gd
@@ -1,9 +1,13 @@
 extends Area2D
-
+@onready var questui1 = preload("res://billboard_ui.tscn")
 @onready var interactable: Area2D = $interactables
-
+var questui
 func _ready() -> void:
 	interactable.interact = _on_interact
+	questui = questui1.instantiate()
+	add_child(questui)
+	questui.hide()
+	questui.visible = false
 
 func _on_interact():
-	print("open billboard yes")
+	questui.show()
