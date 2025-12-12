b6f87ff00ef82203ac9072eb13b8e4c5054f6b18
turdle <shuruiguo1@gmail.com>
2025-12-12 23:44:27 +1100

    shooting in 2 directions for zombie minigame + music + new art

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,7 +2,11 @@ extends CharacterBody2D
 var initial_p = Vector2(1.0, 0.0)
 var notfloor = 0.0
 var gravity = 2000.0
+var notcooldown
+var started
+@onready var timer = $Timer
 @onready var dagger_scene = preload("res://dagger.tscn")
+@onready var leftdagger_scene = preload("res://dagger_left.tscn")
 func _physics_process(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("right"):
@@ -21,11 +25,29 @@ func _physics_process(delta):
 		velocity.y = e.jump
 	if  Input.is_action_just_pressed("attack") and e.allowdagger:
 		shoot()
+	if Input.is_action_just_pressed("left_attack") and e.allowdagger:
+		shoot_left()
 	move_and_slide()
 func _ready():
 	if not e.playerdialogue:
 		DialogueManager.show_dialogue_balloon(load("res://player.dialogue"), "start")
 		e.playerdialogue = true
 func shoot():
-	var dagger = dagger_scene.instantiate()
-	add_child(dagger)
+	if notcooldown:
+		var dagger = dagger_scene.instantiate()
+		add_child(dagger)
+		notcooldown = false
+		started = false
+func shoot_left():
+	if notcooldown:
+		var left_dagger = leftdagger_scene.instantiate()
+		add_child(left_dagger)
+		notcooldown = false
+		started = false
+func _process(_float) -> void:
+	if not notcooldown and not started:
+		timer.start()
+		started = true
+
+func _on_timer_timeout() -> void:
+	notcooldown = true
