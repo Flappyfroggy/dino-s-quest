68fe77755581bc6276b65d15aebfd07a19c934e2
turdle <shuruiguo1@gmail.com>
2025-12-15 21:27:50 +1100

    more animations

--- penguin.gd
+++ penguin.gd
@@ -1,7 +1,9 @@
 extends Area2D
 @onready var interactable: Area2D = $interactables
+@onready var sprite = $Sprite2D
 func _ready():
 	interactable.interact = _on_interact
+	sprite.play("default")
 
 func _on_interact():
 	if e.finish_dialogue:
