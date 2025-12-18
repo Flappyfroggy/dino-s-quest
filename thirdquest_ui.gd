9705a10fdc971f0db9985386ac047d9d11f4712d
turdle <shuruiguo1@gmail.com>
2025-12-18 20:49:55 +1100

    forgot to save these changes w last commit

--- thirdquest_ui.gd
+++ thirdquest_ui.gd
@@ -1,7 +1,8 @@
 extends Control
 @onready var t = $AnimationPlayer
-
+@onready var c = $CanvasLayer
 func _on_acceptbutton_pressed() -> void:
+	c.hide()
 	t.play("new_animation")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
